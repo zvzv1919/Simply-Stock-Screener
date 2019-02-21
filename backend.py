dface6d7d3effad33a12a65793f88eed4f4ca405
Atlowell <atlowell42@gmail.com>
2019-02-21 18:45:51 -0500

    Restructures backend to have proper functions

--- backend.py
+++ backend.py
@@ -48,10 +48,15 @@ import certifi
 import requests
 import csv
 
+# control statement
 
-data = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&'
+# def get_historical_price(time_frame):
+#     if(time_frame=='')
+#get_current('AAL')
+
+"""data = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&'
                     'apikey=PTF07M1M1UTX6RCF&datatype=csv')
-
+"""
 # with open('out.csv', 'w') as f:
 #     writer = csv.writer(f)
 #     reader = csv.reader(data.text.splitlines())
@@ -63,7 +68,7 @@ data = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAIL
 company_list=[]
 
 # update the database
-def update():
+"""def update():
     with open('companylist.csv') as csv_file:
         csv_reader = csv.reader(csv_file)
         line_count = 0
@@ -84,23 +89,47 @@ def update():
         reader = csv.reader(data.text.splitlines())
         for row in reader:
             print row
+"""
 
-def get_intraday(stock_symbol):
+"""def get_intraday(stock_symbol):
     request = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=%s&interval=1min&' \
               'outputsize=full&apikey=PTF07M1M1UTX6RCF&datatype=csv' % stock_symbol
     data = requests.get(request)
     reader = csv.reader(data.text.splitlines())
     for row in reader:
         print row
+"""
 
+# Get stock info (current day)
 def get_current(stock_symbol):
-    request = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=%s' \
+    """request = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=%s' \
               '&apikey=PTF07M1M1UTX6RCF&datatype=csv' % stock_symbol
     data = requests.get(request)
     reader = csv.reader(data.text.splitlines())
     for row in reader:
         print row
+    """
 
-# def get_historical_price(time_frame):
-#     if(time_frame=='')
-get_current('AAL')
+# Get stock info (daily price over time)
+def get_daily(stock_symbol):
+    #api request to retrieve daily price - Xuan
+    # update database with price for each day - Brian
+
+# Retrieve current data for single stock, return it to be displayed, and update database with historical daily price info
+def get_singlestock(stock_symbol):
+    # if data does not already exist in database - Brian
+        get_daily(stock_symbol)
+        # get_current(stock_symbol)?
+        # print current - Xuan
+    # else
+        get_current(stock_symbol)
+        # print current data - Xuan
+    
+
+# Return price points for graph
+def graph(stock_symbol,  timeframe):
+    # Take time frame and grab 30 evenly-spaced data points - Xuan
+    # Retrieve those data points from database x30 - Brian
+    # print 30 data points - Xuan
+
+
