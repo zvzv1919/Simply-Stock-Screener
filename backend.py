508036c4826c5375dba8fd5191e9560c39794c1a
zvzv1919 <zvzv1919@gmail.com>
2019-02-21 17:59:30 -0500

    Added functions to retrieve intraday/current data. also a function to update the database, but wouldn't work because of the API call restrictions

--- backend.py
+++ backend.py
@@ -3,31 +3,104 @@ from StringIO import StringIO
 import pycurl
 import certifi
 
-buffer = StringIO()
-c = pycurl.Curl()
 
-#Place the API request here
-c.setopt(c.URL, 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&'
-                'outputsize=full&apikey=PTF07M1M1UTX6RCF')
-c.setopt(c.CAINFO, certifi.where())
-c.setopt(c.WRITEDATA, buffer)
-c.perform()
-c.close()
+# buffer = StringIO()
+# c = pycurl.Curl()
+#
+# #Place the API request here
+# c.setopt(c.URL, 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&'
+#                 'outputsize=full&apikey=PTF07M1M1UTX6RCF')
+# c.setopt(c.CAINFO, certifi.where())
+# c.setopt(c.WRITEDATA, buffer)
+# c.perform()
+# c.close()
+#
+# #respond from Alpha Vantage in JSON form
+# body = buffer.getvalue()
+# # Body is a string in some encoding.
+# # In Python 2, we can print it without knowing what the encoding is.
+# print(body[:10000])
+#
+# buffer2=StringIO()
+# c = pycurl.Curl()
+# c.setopt(c.URL, 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&'
+#                 'outputsize=full&apikey=PTF07M1M1UTX6RCF')
+# c.setopt(c.CAINFO, certifi.where())
+# c.setopt(c.WRITEDATA, buffer2)
+# c.perform()
+# c.close()
+#
+# body2 = buffer2.getvalue()
+# print(body2[:10000])
 
-#respond from Alpha Vantage in JSON form
-body = buffer.getvalue()
-# Body is a string in some encoding.
-# In Python 2, we can print it without knowing what the encoding is.
-print(body[:10000])
+# buffer3=StringIO()
+# # c = pycurl.Curl()
+# # c.setopt(c.URL, 'http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&render=download'
+# # )
+# # c.setopt(c.CAINFO, certifi.where())
+# # c.setopt(c.WRITEDATA, buffer3)
+# # c.perform()
+# # c.close()
+# #
+# # body3 = buffer3.getvalue()
+# # print(body3[:10000])
 
-buffer2=StringIO()
-c = pycurl.Curl()
-c.setopt(c.URL, 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&'
-                'outputsize=full&apikey=PTF07M1M1UTX6RCF')
-c.setopt(c.CAINFO, certifi.where())
-c.setopt(c.WRITEDATA, buffer2)
-c.perform()
-c.close()
+import requests
+import csv
 
-body2 = buffer2.getvalue()
-print(body2[:10000])
+
+data = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&'
+                    'apikey=PTF07M1M1UTX6RCF&datatype=csv')
+
+# with open('out.csv', 'w') as f:
+#     writer = csv.writer(f)
+#     reader = csv.reader(data.text.splitlines())
+#
+#     for row in reader:
+#         print row[0]
+
+# a list holding all company symbols
+company_list=[]
+
+# update the database
+def update():
+    with open('companylist.csv') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                #print row[0]
+                company_list.append(row[0])
+                line_count += 1
+    print company_list
+    #TODO: complete the testing and intraday data filling
+    for symbol in company_list:
+        request='https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=%s&' \
+                'apikey=PTF07M1M1UTX6RCF&datatype=csv' % symbol
+        print request
+        data=requests.get(request)
+        reader = csv.reader(data.text.splitlines())
+        for row in reader:
+            print row
+
+def get_intraday(stock_symbol):
+    request = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=%s&interval=1min&' \
+              'outputsize=full&apikey=PTF07M1M1UTX6RCF&datatype=csv' % stock_symbol
+    data = requests.get(request)
+    reader = csv.reader(data.text.splitlines())
+    for row in reader:
+        print row
+
+def get_current(stock_symbol):
+    request = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=%s' \
+              '&apikey=PTF07M1M1UTX6RCF&datatype=csv' % stock_symbol
+    data = requests.get(request)
+    reader = csv.reader(data.text.splitlines())
+    for row in reader:
+        print row
+
+# def get_historical_price(time_frame):
+#     if(time_frame=='')
+get_current('AAL')
